23ec20804fdaabb6662ba93dbf981bd4faa5314b
kaihui.wang <hpuwang@gmail.com>
2026-03-06 16:20:01 +0800

    refactor: remove process order cron and worker files, clean up unused code

--- modules/system/worker/consts/const.go
+++ modules/system/worker/consts/const.go
@@ -9,12 +9,7 @@ package consts
 var (
 	URL_CRON       = "url_cron"
 	CMD_CRON       = "cmd_cron"
-	TEST_TASK      = "test_task" //测试任务
 	TEST_CRON      = "test_cron"
-	TEST_TASK_TASK = //测试定时任务
-	"test_task_task"
-	TEST_CRON_CRON     = "test_cron_cron"
-	SEND_EMAIL_CRON    = "send_email_cron"
-	SEND_EMAIL_TASK    = "send_email_task"
-	PROCESS_ORDER_CRON = "process_order_cron"
+	TEST_TASK      = "test_task"      //测试任务
+	TEST_TASK_TASK = "test_task_task" //测试定时任务
 )
