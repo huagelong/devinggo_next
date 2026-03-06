b2edee7f26343c5951ab7ee4bf17a886e44ab8a7
kaihui.wang <hpuwang@gmail.com>
2026-03-06 16:17:30 +0800

    fix: correct formatting and alignment of constants in const.go

--- modules/system/worker/consts/const.go
+++ modules/system/worker/consts/const.go
@@ -7,13 +7,14 @@
 package consts
 
 var (
-	URL_CRON	= "url_cron"
-	CMD_CRON	= "cmd_cron"
-	TEST_TASK	= "test_task"	//测试任务
-	TEST_CRON	= "test_cron"
-	TEST_TASK_TASK	=	//测试定时任务
+	URL_CRON       = "url_cron"
+	CMD_CRON       = "cmd_cron"
+	TEST_TASK      = "test_task" //测试任务
+	TEST_CRON      = "test_cron"
+	TEST_TASK_TASK = //测试定时任务
 	"test_task_task"
-	TEST_CRON_CRON	= "test_cron_cron"
-	SEND_EMAIL_CRON	= "send_email_cron"
-	SEND_EMAIL_TASK	= "send_email_task"
+	TEST_CRON_CRON     = "test_cron_cron"
+	SEND_EMAIL_CRON    = "send_email_cron"
+	SEND_EMAIL_TASK    = "send_email_task"
+	PROCESS_ORDER_CRON = "process_order_cron"
 )
